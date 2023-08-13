5a3da50cdd86a1531396e7632b82933462aad882
Shervin Tajdini <shervin.taj@gmail.com>
2023-08-13 11:36:04 -0400

    implementing toglle using context

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState, createContext, useContext } from 'react';
+
+const languages = ['JavaScript', 'Python'];
+
+const LanguageContext = createContext({
+  favoriteLanguage: languages[0],
+  toggleLanguage: () => {},
+});
+
+export  default function App() {
+  const [favoriteLanguage, setFavoriteLanguage] = useState(languages[0]);
+
+  return (
+    <LanguageContext.Provider value={{ favoriteLanguage, toggleLanguage: () => {
+      const currentIndex = languages.indexOf(favoriteLanguage);
+      const newIndex = (currentIndex + 1) % languages.length;
+      setFavoriteLanguage(languages[newIndex]);
+    }}}>
+      <MainSection />
+    </LanguageContext.Provider>
+  );
+}
+
+function MainSection() {
+  const { favoriteLanguage, toggleLanguage } = useContext(LanguageContext);
+
+  return (
+    <div>
+      <p id="favoriteLanguage">favorite programming language: {favoriteLanguage}</p>
+      <button id="changeFavorite" onClick={toggleLanguage}>
+        toggle language
+      </button>
+    </div>
+  )
+}
+
+
